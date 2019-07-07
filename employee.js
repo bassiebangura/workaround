3283c9a3a47ca132e44dd3fdb171ae5427ba2fc6
bassiebangura <devbassiebangura@gmail.com>
2019-07-07 19:58:36 -0400

    Create Employee module.

--- employee.js
+++ employee.js
@@ -1,51 +1,61 @@
-let salary = 100000;
+let Employee = {
+  salary: 100000,
+  getCadre() {
+    if (
+      Employee.salary >= payGrades.entryLevel.minSalary &&
+      Employee.salary <= payGrades.entryLevel.maxSalary
+    ) {
+      return "entryLevel";
+    } else if (
+      Employee.salary >= payGrades.midLevel.minSalary &&
+      Employee.salary <= payGrades.midLevel.maxSalary
+    ) {
+      return "midLevel";
+    } else return "seniorLevel";
+  },
 
-let payGrades = {
-  entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
-  midLevel: { taxMultiplier: .1, benefits: ['health', 'housing'], minSalary: 50000, maxSalary: 99999 },
-  seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
+  calculateTax() {
+    return payGrades[getCadre()].taxMultiplier * Employee.salary;
+  },
+  getBenefits() {
+    return payGrades[getCadre()].benefits.join(", ");
+  },
+  calculateBonus() {
+    return 0.02 * Employee.salary;
+  },
+  reimbursementEligibility() {
+    let reimbursementCosts = {
+      health: 5000,
+      housing: 8000,
+      wellness: 6000,
+      gym: 12000
+    };
+    let totalBenefitsValue = 0;
+    let employeeBenefits = payGrades[getCadre()].benefits;
+    for (let i = 0; i < employeeBenefits.length; i++) {
+      totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
+    }
+    return totalBenefitsValue;
+  }
 };
 
-function getCadre() {
-  if (salary >= payGrades.entryLevel.minSalary && salary <= payGrades.entryLevel.maxSalary) {
-    return 'entryLevel';
-  } else if (salary >= payGrades.midLevel.minSalary && salary <= payGrades.midLevel.maxSalary) {
-    return 'midLevel';
-  } else return 'seniorLevel';
-}
-
-function calculateTax() {
-  return payGrades[getCadre()].taxMultiplier * salary;
-}
-
-function getBenefits() {
-  return payGrades[getCadre()].benefits.join(', ');
-}
-
-function calculateBonus() {
-  return .02 * salary;
-}
-
-function reimbursementEligibility() {
-  let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
-  let totalBenefitsValue = 0; 
-  let employeeBenefits = payGrades[getCadre()].benefits;
-  for (let i = 0; i < employeeBenefits.length; i++) {
-    totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
+let payGrades = {
+  entryLevel: {
+    taxMultiplier: 0.05,
+    benefits: ["health"],
+    minSalary: 10000,
+    maxSalary: 49999
+  },
+  midLevel: {
+    taxMultiplier: 0.1,
+    benefits: ["health", "housing"],
+    minSalary: 50000,
+    maxSalary: 99999
+  },
+  seniorLevel: {
+    taxMultiplier: 0.2,
+    benefits: ["health", "housing", "wellness", "gym"],
+    minSalary: 100000,
+    maxSalary: 200000
   }
-  return totalBenefitsValue;
-}
-
-function getEmployeeInformation(inputSalary) {
-  salary = inputSalary;
-  console.log('Cadre: ' + getCadre());
-  console.log('Tax: ' + calculateTax());
-  console.log('Benefits: ' + getBenefits());
-  console.log('Bonus: ' + calculateBonus());
-  console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
-}
-
-getEmployeeInformation(10000);
-getEmployeeInformation(50000);
-getEmployeeInformation(100000);
-
+};
--- workaround.js
+++ workaround.js
@@ -1,3 +1,16 @@
 //WorkAround is a human resources program that 
 //returns basic information about an employee, 
 //based on salary data.
+
+function getEmployeeInformation(inputSalary) {
+    salary = inputSalary;
+    console.log('Cadre: ' + getCadre());
+    console.log('Tax: ' + calculateTax());
+    console.log('Benefits: ' + getBenefits());
+    console.log('Bonus: ' + calculateBonus());
+    console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
+  }
+  
+  getEmployeeInformation(10000);
+  getEmployeeInformation(50000);
+  getEmployeeInformation(100000);
